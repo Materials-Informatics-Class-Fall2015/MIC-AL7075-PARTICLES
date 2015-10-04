9cd8adc54db34d8cdc69741368f21023c9c4ca99
Christopher Shartrand <cshartrand@gatech.edu>
2015-10-04 15:13:14 -0400

    shut up github

---
+++ structure_processing/ReadResponses.py
@@ -0,0 +1,39 @@
+import os
+import pymks
+from pymks import PrimitiveBasis
+from pymks.stats import correlate
+from pymks.tools import draw_correlations
+import numpy as np
+
+def readDirectory(directory):
+    filenames=[]
+    for f in os.listdir(directory):
+        if(f.find("s_el_max")!=-1):
+            filenames.append(f)
+    strain_list = [0]*len(filenames)
+    for f in filenames:
+        num = int(f[f.rfind("_")+1:f.rfind(".csv")])
+        elem_f = os.path.join(directory, "trial_elem_grains_%d.txt" % num)
+        elem_f = open(elem_f, "r")
+        elem_f.readline()
+        line = elem_f.readline()
+        shape = map(int, line.split(",")[:3])
+        f = os.path.join(directory, f)
+        strains = np.asarray([])
+        strain_f = open(f,"r")
+        for line in strain_f:
+        	strains = np.append(strains,float(line.split(",")[4]))
+        strains = np.reshape(strains, shape, order='F')
+        elem_f.close()
+        strain_list[num] = strains
+    return strain_list
+    
+
+
+if __name__ == "__main__":
+    import sys
+    dir = os.getcwd()
+    if(os.path.isdir(sys.argv[-1])):
+        dir = sys.argv[-1]
+    ms_list = readDirectory(dir)
+    plotCorrelations(ms_list)
---
+++ test.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit as tm
+
+n = 9
+center = (n - 1) / 2
+
+from pymks.tools import draw_microstructures
+from pymks.datasets import make_delta_microstructures
+
+X_delta = make_delta_microstructures(n_phases=2, size=(n, n, n))
+draw_microstructures(X_delta[:, center])
+
+from pymks.datasets import make_elastic_FE_strain_delta
+from pymks.tools import draw_microstructure_strain
+
+elastic_modulus = (80, 120)
+poissons_ratio = (0.3, 0.3)
+macro_strain = 0.02
+size = (n, n, n)
+
+t = tm.time.time()
+X_delta, strains_delta = make_elastic_FE_strain_delta(elastic_modulus=elastic_modulus,
+                                                      poissons_ratio=poissons_ratio,
+                                                      size=size, macro_strain=macro_strain)
+print 'Elapsed Time',tm.time.time() - t, 'Seconds'
+
+draw_microstructure_strain(X_delta[0, center, :, :], strains_delta[0, center, :, :])
+from pymks import MKSLocalizationModel
+from pymks.bases import PrimitiveBasis
+
+prim_basis = PrimitiveBasis(n_states=2)
+model = MKSLocalizationModel(basis=prim_basis)
+
+model.fit(X_delta, strains_delta)
+
+from pymks.tools import draw_coeff
+
+coeff = model.coeff
+draw_coeff(coeff[center])
+
+from pymks.datasets import make_elastic_FE_strain_random
+
+np.random.seed(99)
+t = tm.time.time()
+X, strain = make_elastic_FE_strain_random(n_samples=1, elastic_modulus=elastic_modulus,
+                                          poissons_ratio=poissons_ratio, size=size, macro_strain=macro_strain)
+print 'Elapsed Time',(tm.time.time() - t), 'Seconds'
+draw_microstructure_strain(X[0, center] , strain[0, center])
+
+t = tm.time.time()
+strain_pred = model.predict(X)
+print 'Elapsed Time',tm.time.time() - t,'Seconds'
+
+from pymks.tools import draw_strains_compare
+
+draw_strains_compare(strain[0, center], strain_pred[0, center])
+
+from pymks.tools import draw_differences
+
+draw_differences([strain[0, center] - strain_pred[0, center]], ['Finite Element - MKS'])
